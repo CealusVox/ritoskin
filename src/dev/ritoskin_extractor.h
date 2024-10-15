da57ebf9252053f45914c63a1c77082bcb9db696
nylish <nylish.me@gmail.com>
2024-10-16 00:35:08 +0300

    Update ritoskin_extractor.h

--- src/dev/ritoskin_extractor.h
+++ src/dev/ritoskin_extractor.h
@@ -16,12 +16,13 @@
 namespace fs = std::filesystem;
 using json = nlohmann::json;
 
+std::vector<fs::path> find_related_folders(const fs::path& champion_folder);
+void process_champion_folder(const fs::path& champion_folder);
+void process_related_folder(const fs::path& related_folder, const fs::path& extracted_skins_folder, const std::string& champion_name);
+void process_bin_file(const fs::path& bin_file_path, const fs::path& output_folder, int skin_number);
 void convert_bin_to_py(const fs::path& file_path);
 void modify_py_file(const fs::path& file_path);
 void convert_py_to_bin(const fs::path& file_path);
-void process_bin_file(const fs::path& bin_file_path, const fs::path& output_folder, int skin_number);
-void process_champion_folder(const fs::path& champion_folder);
-bool find_bin_file(const fs::path& dir, const std::string& filename, fs::path& result);
 
 #endif
 
